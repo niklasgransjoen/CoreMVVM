379f77e454756357c5d9e555552ceee2b50e0a4b
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-06-26 22:44:25 +0200

    Added file I forgot about

---
+++ CoreMVVM.Demo/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace CoreMVVM.Demo.Views
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ILogger _logger;
+
+        public MainWindow(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            ((ScreenPrinter)_logger).RegisterTextBox(textBox);
+        }
+
+        private void Debug_Click(object sender, RoutedEventArgs e)
+        {
+            _logger.Debug("Debug action performed. Result: success.");
+        }
+
+        private void Log_Click(object sender, RoutedEventArgs e)
+        {
+            _logger.Log("That button click was kind of important, and has been logged.");
+        }
+
+        private void Error_Click(object sender, RoutedEventArgs e)
+        {
+            _logger.Error("Clicking that button sent me into a state I shouldn't have been in. That's called an error.");
+        }
+
+        private void ExceptionClick(object sender, RoutedEventArgs e)
+        {
+            _logger.Exception("An exception was thrown by clicking that button!", new Exception("Don't click the exception button, it will throw exceptions!"));
+        }
+    }
+}
