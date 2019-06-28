6e131fa11c41e6246e9a4b9f34f9c6d0619802a9
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-06-28 22:50:38 +0200

    Cleaned up ContainerBuilderTests.cs

--- Tests/CoreMVVM.Tests/IOC/Builder/ContainerBuilderTests.cs
+++ Tests/CoreMVVM.Tests/IOC/Builder/ContainerBuilderTests.cs
@@ -8,48 +8,50 @@ namespace CoreMVVM.Tests.IOC.Builder
     [TestFixture]
     public class ContainerBuilderTests
     {
+        private ContainerBuilder _builder;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _builder = new ContainerBuilder(registerDefaults: false);
+        }
+
         [TestCaseSource(nameof(GetTypes))]
         public void Builder_Registers_Types(Type type)
         {
-            ContainerBuilder builder = new ContainerBuilder(registerDefaults: false);
-            RegistrationBuilder regBuilder = builder.Register(type);
-
-            Assert.AreEqual(type, regBuilder.Type);
-            Assert.IsFalse(regBuilder.IsSingleton);
-            Assert.IsEmpty(regBuilder.Registrations);
+            RegistrationBuilder regBuilder = _builder.Register(type);
+            ValidateRegistrationBuilder(regBuilder, type, isSingleton: false);
         }
 
         [TestCaseSource(nameof(GetTypes))]
         public void Builder_Registers_Types_AsSingletons(Type type)
         {
-            ContainerBuilder builder = new ContainerBuilder(registerDefaults: false);
-            RegistrationBuilder regBuilder = builder.RegisterSingleton(type);
-
-            Assert.AreEqual(type, regBuilder.Type);
-            Assert.IsTrue(regBuilder.IsSingleton);
-            Assert.IsEmpty(regBuilder.Registrations);
+            RegistrationBuilder regBuilder = _builder.RegisterSingleton(type);
+            ValidateRegistrationBuilder(regBuilder, type, isSingleton: true);
         }
 
         [Test]
         public void Builder_Registers_Generics()
         {
-            ContainerBuilder builder = new ContainerBuilder(registerDefaults: false);
-            RegistrationBuilder regBuilder = builder.Register<IInterface>();
-
-            Assert.AreEqual(typeof(IInterface), regBuilder.Type);
-            Assert.IsFalse(regBuilder.IsSingleton);
-            Assert.IsEmpty(regBuilder.Registrations);
+            RegistrationBuilder regBuilder = _builder.Register<IInterface>();
+            ValidateRegistrationBuilder(regBuilder, typeof(IInterface), isSingleton: false);
         }
 
         [Test]
         public void Builder_Registers_Generics_AsSingleton()
         {
-            ContainerBuilder builder = new ContainerBuilder(registerDefaults: false);
-            RegistrationBuilder regBuilder = builder.RegisterSingleton<Class>();
+            RegistrationBuilder regBuilder = _builder.RegisterSingleton<Class>();
+            ValidateRegistrationBuilder(regBuilder, typeof(Class), isSingleton: true);
+        }
 
-            Assert.AreEqual(typeof(Class), regBuilder.Type);
-            Assert.IsTrue(regBuilder.IsSingleton);
-            Assert.IsEmpty(regBuilder.Registrations);
+        private void ValidateRegistrationBuilder(RegistrationBuilder regBuilder, Type expectedType, bool isSingleton)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedType, regBuilder.Type);
+                Assert.AreEqual(isSingleton, regBuilder.IsSingleton);
+                Assert.IsEmpty(regBuilder.Registrations);
+            });
         }
 
         private static IEnumerable<Type> GetTypes()
---
+++ Tests/CoreMVVM.Tests/IOC/Builder/RegistrationBuilderTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace CoreMVVM.Tests.IOC.Builder
+{
+    [TestFixture]
+    public class RegistrationBuilderTests
+    {
+    }
+}
