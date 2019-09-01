8fddbbd1a81e1956c253508a2d6ccd237976f630
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-09-01 16:38:17 +0200

    Updated CoreMVVM.Demo project

---
+++ CoreMVVM.Demo/DialogWindowModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+
+namespace CoreMVVM.Demo
+{
+    /*
+     * ViewModel not located in folder ViewModels.
+     * Will still be resolved to DialogWindow.xaml because it's been registered with the ViewLocator.
+     */
+
+    public class DialogWindowModel : BaseModel
+    {
+        public DialogWindowModel()
+        {
+            ButtonCommand = new RelayCommand<string>(OnButtonPressed);
+        }
+
+        #region Commands
+
+        public ICommand ButtonCommand { get; }
+
+        private void OnButtonPressed(string text)
+        {
+            Status = $"The current content of the textbox is \"{text}\".";
+        }
+
+        #endregion Commands
+
+        #region Properties
+
+        private string _status;
+
+        public string Status
+        {
+            get => _status;
+            set => SetProperty(ref _status, value);
+        }
+
+        #endregion Properties
+    }
+}
--- CoreMVVM.Demo/ViewModels/MainWindowModel.cs
+++ CoreMVVM.Demo/ViewModels/MainWindowModel.cs
@@ -1,11 +1,72 @@
-﻿namespace CoreMVVM.Demo.ViewModels
+﻿using CoreMVVM.Windows;
+using System;
+using System.Windows.Input;
+
+namespace CoreMVVM.Demo.ViewModels
 {
     internal class MainWindowModel : BaseModel
     {
-        public MainWindowModel(SinglePageViewModel content)
+        private readonly ILogger _logger;
+        private readonly IWindowManager _windowManager;
+
+        public MainWindowModel(
+            SinglePageViewModel content,
+            ILogger logger,
+            IWindowManager windowManager)
         {
             Content = content;
+            _logger = logger;
+            _windowManager = windowManager;
+
+            DebugCommand = new RelayCommand(OnDebug);
+            LogCommand = new RelayCommand(OnLog);
+            ErrorCommand = new RelayCommand(OnError);
+            ExceptionCommand = new RelayCommand(OnException);
+
+            ShowDialogCommand = new RelayCommand(OnShowDialog);
         }
+
+        #region Commands
+
+        public ICommand DebugCommand { get; }
+        public ICommand LogCommand { get; }
+        public ICommand ErrorCommand { get; }
+        public ICommand ExceptionCommand { get; }
+
+        private void OnDebug()
+        {
+            _logger.Debug("Debug action performed. Result: success.");
+        }
+
+        private void OnLog()
+        {
+            _logger.Log("That button click was kind of important, and has been logged.");
+        }
+
+        private void OnError()
+        {
+            _logger.Error("Clicking that button sent me into a state I shouldn't have been in. That's called an error.");
+        }
+
+        private void OnException()
+        {
+            _logger.Exception("An exception was thrown by clicking that button!", new Exception("Don't click the exception button, it will throw exceptions!"));
+        }
+
+        #region ShowDialog
+
+        public ICommand ShowDialogCommand { get; }
+
+        private void OnShowDialog()
+        {
+            _windowManager.ShowDialog<DialogWindowModel>();
+        }
+
+        #endregion ShowDialog
+
+        #endregion Commands
+
+        #region Properties
 
         private object _content;
 
@@ -14,5 +75,7 @@
             get => _content;
             set => SetProperty(ref _content, value);
         }
+
+        #endregion Properties
     }
 }
