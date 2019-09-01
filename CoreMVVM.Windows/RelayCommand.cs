cf1e5825a0008bd900c46976e839c422cab7d5d0
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-09-01 16:08:57 +0200

    Added ICommand implementation RelayCommand

---
+++ CoreMVVM.Windows/RelayCommand.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CoreMVVM
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        private readonly Action<object> _execute = null;
+        private readonly Func<object, bool> _canExecute = null;
+
+        private event EventHandler _canExecuteChanged;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute)
+        {
+            if (execute is null)
+                throw new ArgumentNullException(nameof(execute));
+
+            _execute = param => execute();
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = param => execute();
+            _canExecute = param => canExecute();
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<object, bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = param => execute();
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<object> execute, Func<bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = execute;
+            _canExecute = param => canExecute();
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion Constructors
+
+        #region ICommand Members
+
+        ///<summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        ///</summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                _canExecuteChanged += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                _canExecuteChanged -= value;
+            }
+        }
+
+        /// <summary>
+        /// Returns a value indicating if this command can be executed.
+        /// </summary>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        /// <summary>
+        /// Executes this command.
+        /// </summary>
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        #endregion ICommand Members
+
+        public void RaiseCanExecute()
+        {
+            _canExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        #region Fields
+
+        private readonly Action<T> _execute = null;
+        private readonly Func<T, bool> _canExecute = null;
+
+        private event EventHandler _canExecuteChanged;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute)
+        {
+            if (execute is null)
+                throw new ArgumentNullException(nameof(execute));
+
+            _execute = param => execute();
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<T> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = param => execute();
+            _canExecute = param => canExecute();
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<T, bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = param => execute();
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Func<bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = execute;
+            _canExecute = param => canExecute();
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute));
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute));
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion Constructors
+
+        #region ICommand Members
+
+        ///<summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        ///</summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                _canExecuteChanged += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                _canExecuteChanged -= value;
+            }
+        }
+
+        /// <summary>
+        /// Returns a value indicating if this command can be executed.
+        /// </summary>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke((T)parameter) ?? true;
+        }
+
+        ///<summary>
+        /// Executes this command.
+        ///</summary>
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        #endregion ICommand Members
+
+        public void RaiseCanExecute()
+        {
+            _canExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
