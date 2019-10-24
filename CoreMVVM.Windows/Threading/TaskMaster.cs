b2062578cd65db9219363867e1142b300037ca33
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-10-24 10:41:35 +0200

    Implemented task master for switching threads
    This resolves #38.

---
+++ CoreMVVM.Windows/Threading/TaskMaster.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CoreMVVM.Windows.Threading
+{
+    /// <summary>
+    /// A tool for switching between threads.
+    /// </summary>
+    public static class TaskMaster
+    {
+        /// <summary>
+        /// Switches to the UI thread, by using the dispatcher of the current application.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"><see cref="Application.Current"/> is null.</exception>
+        public static DispatcherThreadSwitcher AwaitUIThread()
+        {
+            if (Application.Current is null)
+                throw new InvalidOperationException("Cannot await UI thread when Application.Current is null");
+
+            return AwaitThread(Application.Current.Dispatcher);
+        }
+
+        /// <summary>
+        /// Switches to the thread of the given dispatcher.
+        /// </summary>
+        /// <param name="dispatcher">The dispatcher to switch to.</param>
+        public static DispatcherThreadSwitcher AwaitThread(Dispatcher dispatcher)
+        {
+            return new DispatcherThreadSwitcher(dispatcher);
+        }
+
+        /// <summary>
+        /// Switches to a new background thread.
+        /// </summary>
+        public static ThreadPoolThreadSwitcher AwaitBackgroundThread()
+        {
+            return new ThreadPoolThreadSwitcher();
+        }
+    }
+}
---
+++ CoreMVVM.Windows/Threading/ThreadPoolThreadSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace CoreMVVM.Windows.Threading
+{
+    /// <summary>
+    /// A task-like structure used to switch context to a new thread.
+    /// </summary>
+    public struct ThreadPoolThreadSwitcher : INotifyCompletion
+    {
+        public ThreadPoolThreadSwitcher GetAwaiter() => this;
+
+        public bool IsCompleted => false;
+
+        public void GetResult()
+        { }
+
+        public void OnCompleted(Action continuation) =>
+            ThreadPool.QueueUserWorkItem(_ => continuation());
+    }
+}
