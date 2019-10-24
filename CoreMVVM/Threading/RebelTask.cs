382f39c1f2327d26fdf097f62a6f3d1116b836ae
Niklas Gransjøen <niklasgransjoen@gmail.com>
2019-10-24 22:19:59 +0200

    Added custom task type RebelTask
    This struct normally wraps a standard Task, but configures with to not
    continue on the captured context as default.
    
    This also completed the .NET Core 3.0 port, and resolves #35.

---
+++ CoreMVVM/Threading/RebelTask.cs
@@ -0,0 +1,224 @@
+﻿using CoreMVVM.CompilerServices;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace CoreMVVM.Threading
+{
+    /// <summary>
+    /// A task that does not continue on the captured context as default.
+    /// </summary>
+    [AsyncMethodBuilder(typeof(RebelTaskMethodBuilder))]
+    public readonly struct RebelTask
+    {
+        private readonly Task _task;
+
+        #region Constructors
+
+        public RebelTask(Task task)
+        {
+            _task = task;
+        }
+
+        public static RebelTask<TResult> FromResult<TResult>(TResult result)
+        {
+            return new RebelTask<TResult>(result);
+        }
+
+        #endregion Constructors
+
+        /// <summary>
+        /// Returns a task configured to continue on the captured context.
+        /// </summary>
+        public ConfiguredTaskAwaitable ContinueOnCapturedContext()
+        {
+            Task task = GetTaskOrComplete();
+            return task.ConfigureAwait(true);
+        }
+
+        /// <summary>
+        /// Returns the awaiter of this task, configured to not continue on the captured context.
+        /// </summary>
+        public ConfiguredTaskAwaitable.ConfiguredTaskAwaiter GetAwaiter()
+        {
+            Task task = GetTaskOrComplete();
+            return task.ConfigureAwait(false).GetAwaiter();
+        }
+
+        private Task GetTaskOrComplete()
+        {
+            return _task ?? Task.CompletedTask;
+        }
+
+        #region Operators
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RebelTask task))
+                return false;
+
+            return _task == task._task;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = (hash * 7) + _task?.GetHashCode() ?? 13;
+
+            return hash;
+        }
+
+        public static bool operator ==(RebelTask left, RebelTask right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(RebelTask left, RebelTask right)
+        {
+            return !left.Equals(right);
+        }
+
+        public static implicit operator RebelTask(Task task)
+        {
+            return new RebelTask(task);
+        }
+
+        #endregion Operators
+    }
+
+    /// <summary>
+    /// A task that does not continue on the captured context as default.
+    /// </summary>
+    [AsyncMethodBuilder(typeof(RebelTaskMethodBuilder<>))]
+    public readonly struct RebelTask<TResult> : INotifyCompletion
+    {
+        private readonly Task<TResult> _task;
+        private readonly TResult _result;
+
+        #region Constructors
+
+        public RebelTask(Task<TResult> task)
+        {
+            if (task.IsCompleted)
+            {
+                _task = null;
+                _result = task.Result;
+            }
+            else
+            {
+                _task = task;
+                _result = default;
+            }
+        }
+
+        public RebelTask(TResult result)
+        {
+            _task = null;
+            _result = result;
+        }
+
+        public static RebelTask<T> FromResult<T>(T result)
+        {
+            return new RebelTask<T>(result);
+        }
+
+        #endregion Constructors
+
+        /// <summary>
+        /// Returns a task configured to continue on the captured context.
+        /// </summary>
+        public ConfiguredTaskAwaitable<TResult> ContinueOnCapturedContext()
+        {
+            if (_task is null)
+                return Task.FromResult(_result).ConfigureAwait(true);
+
+            return _task.ConfigureAwait(true);
+        }
+
+        /// <summary>
+        /// Returns the awaiter of this task, configured to not continue on the captured context.
+        /// </summary>
+        public RebelTask<TResult> GetAwaiter() => this;
+
+        #region Awaiter
+
+        /// <summary>
+        /// Gets a value indicating if this task has completed.
+        /// </summary>
+        public bool IsCompleted
+        {
+            get
+            {
+                if (_result != null)
+                    return true;
+
+                return _task.IsCompleted;
+            }
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            if (_result != null)
+                continuation();
+            else
+                _task.ConfigureAwait(false).GetAwaiter().OnCompleted(continuation);
+        }
+
+        /// <summary>
+        /// Returns the result of this task.
+        /// </summary>
+        public TResult GetResult()
+        {
+            if (_result != null)
+                return _result;
+
+            return _task.Result;
+        }
+
+        #endregion Awaiter
+
+        #region Operators
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RebelTask<TResult> task))
+                return false;
+
+            if (_result == null && task._result == null)
+            {
+                return _task == task._task;
+            }
+
+            if (_result == null ^ _result == null)
+                return false;
+
+            return _result.Equals(_result);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 39;
+            hash = (hash * 7) + _result?.GetHashCode() ?? 13;
+            hash = (hash * 7) + _task?.GetHashCode() ?? 13;
+
+            return hash;
+        }
+
+        public static bool operator ==(RebelTask<TResult> left, RebelTask<TResult> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(RebelTask<TResult> left, RebelTask<TResult> right)
+        {
+            return !left.Equals(right);
+        }
+
+        public static implicit operator RebelTask<TResult>(Task<TResult> task)
+        {
+            return new RebelTask<TResult>(task);
+        }
+
+        #endregion Operators
+    }
+}
--- Tests/CoreMVVM.Tests/Threading/RebelTaskTests.cs
+++ Tests/CoreMVVM.Tests/Threading/RebelTaskTests.cs
@@ -6,9 +6,37 @@ namespace CoreMVVM.Threading.Tests
     public sealed class RebelTaskTests
     {
         [Fact]
-        public async RebelTask RebelTask_Executes()
+        public async Task RebelTask_Executes()
         {
-            await Task.Delay(100);
+            await Await1();
+            await Await2();
+
+            string res1 = await Await3();
+            string res2 = await Await4();
+
+            Assert.Equal("JOIN THE ASCENDENCY!", res1);
+            Assert.Equal("WHAT'S UP DANGER", res2);
+        }
+
+        private RebelTask Await1()
+        {
+            return Task.Delay(1);
+        }
+
+        private async RebelTask Await2()
+        {
+            await Task.Yield();
+        }
+
+        private RebelTask<string> Await3()
+        {
+            return RebelTask.FromResult("JOIN THE ASCENDENCY!");
+        }
+
+        private async RebelTask<string> Await4()
+        {
+            await Task.Yield();
+            return "WHAT'S UP DANGER";
         }
     }
 }
