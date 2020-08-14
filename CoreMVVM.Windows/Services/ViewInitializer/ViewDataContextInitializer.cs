3224a9f5e3734cdafc15c840981bbf02bdb2e623
Niklas Gransjøen <niklasgransjoen@gmail.com>
2020-08-14 12:26:01 +0200

    Added IViewInitializer, inject IEnumerable<IViewProvider>

---
+++ CoreMVVM.Windows/Services/ViewInitializer/ViewDataContextInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace CoreMVVM.Windows
+{
+    /// <summary>
+    /// Implementation of <see cref="IViewInitializer"/> that assigns the datacontext if the view is of type <see cref="FrameworkElement"/>.
+    /// </summary>
+    public class ViewDataContextInitializer : IViewInitializer
+    {
+        public virtual void InitView(object viewModel, object view)
+        {
+            if (view is FrameworkElement frameworkElement)
+            {
+                frameworkElement.DataContext = viewModel;
+            }
+        }
+    }
+}
